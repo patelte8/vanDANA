4326b02a2592f31a61c2a257e4969d65e9002a6b
Tejas Patel <46092181+patelte8@users.noreply.github.com>
2022-10-22 15:21:41 -0400

    Update formpi.py

--- src/formpi.py
+++ src/formpi.py
@@ -3,6 +3,7 @@ from dolfin import *
 # Create mesh
 mesh = Mesh("LA_CB.xml")
 boundaries = MeshFunction("size_t", mesh, "LA_CB_facet_region.xml") 
+subdomains = MeshFunction("size_t", mesh, "LA_CB_physical_region.xml")
 
 x = mesh.coordinates()
 scaling_factor = 0.0001
@@ -11,4 +12,4 @@ x[:, :] *= scaling_factor
 hdf = HDF5File(mesh.mpi_comm(), "file_f.h5", "w")
 hdf.write(mesh, "/mesh")
 hdf.write(boundaries, "/boundaries")
-
+hdf.write(subdomains, "/subdomains")
