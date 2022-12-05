93b27cb31ed8aede700ef5fd382ea2be97953a9b
Tejas Patel <46092181+patelte8@users.noreply.github.com>
2022-12-04 19:28:26 -0500

    Update formpi_gmsh.py

--- src/formpi_gmsh.py
+++ src/formpi_gmsh.py
@@ -6,9 +6,9 @@ mesh1 = Mesh("box.xml")
 subdomains1 = MeshFunction("size_t", mesh1, "box_physical_region.xml")
 boundaries1 = MeshFunction("size_t", mesh1, "box_facet_region.xml")
 
-mesh2 = Mesh("flag.xml")
-subdomains2 = MeshFunction("size_t", mesh2, "flag_physical_region.xml")
-boundaries2 = MeshFunction("size_t", mesh2, "flag_facet_region.xml")
+mesh2 = Mesh("flag3D.xml")
+subdomains2 = MeshFunction("size_t", mesh2, "flag3D_physical_region.xml")
+boundaries2 = MeshFunction("size_t", mesh2, "flag3D_facet_region.xml")
 
 
 hdf = HDF5File(mesh1.mpi_comm(), "file_f.h5", "w")
