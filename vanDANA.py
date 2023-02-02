a2bdb70b36a9409940df4a11f08eee77aec1a0e8
Tejas Patel <46092181+patelte8@users.noreply.github.com>
2023-02-02 08:57:01 -0600

    Update vanDANA.py

--- vanDANA.py
+++ vanDANA.py
@@ -92,7 +92,7 @@ def vanDANA_solver(args):
 	if problem_physics['solve_FSI'] == True:
 
 	    print("\nSolid mesh specs | edge length: Max =",hmax_s, "; Min =",hmin_s, flush = True)
-	    print(GREEN % "\nStiffness = {}".format(Sm), flush = True)
+	    print(GREEN % "\nShear modulus = {}".format(Sm), flush = True)
 	    print(GREEN % "Compressiblity = {}".format(Ld), \
 	      BLUE % "; considering compressible solid = {}".format(problem_physics['compressible_solid']), "\n", flush = True)
 	    print(RED % "The following ratios are defined wrt fluid as the reference domain:", flush = True)
