e55841abeb4337bcc5a1cd5ad5353cef718f0012
Tejas Patel <46092181+patelte8@users.noreply.github.com>
2023-03-06 22:14:17 -0600

    Update mesh.py

--- src/mesh.py
+++ src/mesh.py
@@ -1,7 +1,7 @@
 from dolfin import *
 from mshr import *
 
-mesh1 = RectangleMesh(Point(0.0, 0.0), Point(11, 4.1), 700, 260) 
+mesh1 = RectangleMesh(Point(0.0, 0.0), Point(11, 4.1), 500, 185) 
 
 
 class Left(SubDomain):
