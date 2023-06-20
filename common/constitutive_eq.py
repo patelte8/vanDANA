62c1313c78fe7f0e6868b84547496d3dcff7e625
Tejas Patel <46092181+patelte8@users.noreply.github.com>
2023-06-19 23:34:28 -0500

    Update constitutive_eq.py

--- common/constitutive_eq.py
+++ common/constitutive_eq.py
@@ -1,23 +1,6 @@
 from dolfin import sym, nabla_grad, Identity, inv, det, ln
 from .functions import *
 
-# Define fluid stress tensor
-def sigma(Re, u, p):
-    
-    return (2/Re)*sym(nabla_grad(u)) - p*Identity(len(u))
-
-# Define solid stress : incompressible
-def stress_inc(D_R, ps_R, Sm): 
-    
-    B = F(D_R)
-    return -1*(ps_R + Sm)*inv(B) + Sm*B.T 
-
-# Define solid stress matrix : compressible
-def stress_c(D_R, Ld, Sm): 
-    
-    B = F(D_R)
-    return (Ld*ln(J(B)) - Sm)*inv(B) + Sm*B.T    
-
 # Deformation gradient
 def F(x):
     
@@ -27,4 +10,26 @@ def F(x):
 # Jacobian
 def J(F):
     
-    return det(F)    
+    return det(F)   
+
+# Define fluid stress tensor
+# ----------------------------------------------------------
+
+def sigma(Re, u, p):
+    
+    return (2/Re)*sym(nabla_grad(u)) - p*Identity(len(u))
+
+# Neohookean material
+# ----------------------------------------------------------
+
+# Define solid stress : incompressible
+def stress_inc(D_R, ps_R, Sm): 
+    
+    B = F(D_R)
+    return -1*(ps_R + Sm)*inv(B) + Sm*B.T 
+
+# Define solid stress : compressible
+def stress_c(D_R, Ld, Sm): 
+    
+    B = F(D_R)
+    return (Ld*ln(J(B)) - Sm)*inv(B) + Sm*B.T    
