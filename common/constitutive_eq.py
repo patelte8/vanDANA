6256ba9ef35c8517e488684d8000a343db22b9c6
Tejas Patel <46092181+patelte8@users.noreply.github.com>
2023-06-19 23:49:00 -0500

    Update constitutive_eq.py

--- common/constitutive_eq.py
+++ common/constitutive_eq.py
@@ -12,12 +12,40 @@ def J(F):
     
     return det(F)   
 
+# Cauchy's strain tensor
+def E(B, dim):
+    
+    E = B + B.T
+    E *= 0.5
+    E -= Identity(dim)
+    return E
+
 # Define fluid stress tensor
 # ----------------------------------------------------------
 
 def sigma(Re, u, p):
     
     return (2/Re)*sym(nabla_grad(u)) - p*Identity(len(u))
+
+# Linearly elastic material
+# ----------------------------------------------------------
+
+# Define solid stress : incompressible
+def stress_lr_elastic_inc(D_R, ps_R, Sm): 
+    
+    B = F(D_R)
+    E = E(B, D_R.geometric_dimension())
+
+    return (-1*ps_R + 2*Sm*E)*inv(B)  
+
+# Define solid stress : compressible
+def stress_lr_elastic_c(D_R, nw, Sm): 
+    
+    B = F(D_R)
+    E = E(B, D_R.geometric_dimension())
+    lame1 = (2*Sm*nw)/(1-(2*nw))
+
+    return (lame1*tr(E) + 2*Sm*E)*inv(B)
 
 # Neohookean material
 # ----------------------------------------------------------
