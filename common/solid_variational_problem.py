a00256646830edd01ccdc10631ca22450acd7564
Tejas Patel <46092181+patelte8@users.noreply.github.com>
2024-04-09 20:25:07 -0500

    Update solid_variational_problem.py

--- common/solid_variational_problem.py
+++ common/solid_variational_problem.py
@@ -9,8 +9,8 @@ import sys
 
 sys.path.insert(0,  '..')
 from user_inputs.user_parameters import *
-from user_inputs.problem_specific import Shear_modulus
-from user_inputs.boundary_initial_conditions import constrained_domain
+from user_inputs.problem_specific import *
+from user_inputs.boundary_initial_conditions import *
 from utilities.read import *
 
 
