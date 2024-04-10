e58c7d158592c2f1398639ab240ab01bde486af4
Tejas Patel <46092181+patelte8@users.noreply.github.com>
2024-04-09 20:28:53 -0500

    Update flow_temperature_variational_problem.py

--- common/flow_temperature_variational_problem.py
+++ common/flow_temperature_variational_problem.py
@@ -9,8 +9,8 @@ import sys
 
 sys.path.insert(0,  '..')
 from user_inputs.user_parameters import *
-from user_inputs.boundary_initial_conditions import constrained_domain
-from user_inputs.problem_specific import perf, blood_perfusion
+from user_inputs.boundary_initial_conditions import *
+from user_inputs.problem_specific import *
 from utilities.read import *
 
 
