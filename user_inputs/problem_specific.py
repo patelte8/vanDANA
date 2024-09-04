b8182637db6da04500c8695181b2298e479c6473
Tejas Patel <46092181+patelte8@users.noreply.github.com>
2024-09-04 12:24:51 -0500

    Update problem_specific.py

--- user_inputs/problem_specific.py
+++ user_inputs/problem_specific.py
@@ -26,23 +26,3 @@ parabolic_profile = Expression('6.0*x[1]*(4.1 - x[1])/(4.1*4.1)', degree=2)
 def time_varying_bc(tt):
 
 	pass
-
-# ---------------------------------------------------------------------------------  
-
-class Shear_modulus(UserExpression):
-
-	def __init__(self, subdomains, Mat_0, Mat_1, **kwargs):
-		super().__init__(**kwargs)
-		self.subdomains = subdomains
-		self.Mat_0 = Mat_0
-		self.Mat_1 = Mat_1
-
-	def eval_cell(self, values, x, cell):
-		if self.subdomains[cell.index] == 2:
-			values[0] = self.Mat_0
-
-		else:
-			values[0] = self.Mat_1
-
-	def value_shape(self):
-		return ()
